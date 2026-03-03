7b56675707de04921b3704a7223f7256184fa7b0
Brenn <brenn@dontbugme.com>
2026-03-03 07:00:53 +0100

    refactor(capture): delegate accessors and clears to BufferStore

--- internal/capture/buffer_store.go
+++ internal/capture/buffer_store.go
@@ -22,8 +22,8 @@ type BufferStore struct {
 	networkBodyMemoryTotal int64
 
 	// Enhanced action buffer state.
-	enhancedActions []EnhancedAction
-	actionAddedAt   []time.Time
+	enhancedActions  []EnhancedAction
+	actionAddedAt    []time.Time
 	actionTotalAdded int64
 }
 
@@ -33,4 +33,103 @@ func newBufferStore() BufferStore {
 		networkBodies:   make([]NetworkBody, 0, MaxNetworkBodies),
 		enhancedActions: make([]EnhancedAction, 0, MaxEnhancedActions),
 	}
+}
+
+func (s *BufferStore) networkTimestamps() []time.Time {
+	return cloneTimes(s.networkAddedAt)
+}
+
+func (s *BufferStore) webSocketTimestamps() []time.Time {
+	return cloneTimes(s.wsAddedAt)
+}
+
+func (s *BufferStore) actionTimestamps() []time.Time {
+	return cloneTimes(s.actionAddedAt)
+}
+
+func (s *BufferStore) networkBodiesCopy() []NetworkBody {
+	if len(s.networkBodies) == 0 {
+		return []NetworkBody{}
+	}
+	out := make([]NetworkBody, len(s.networkBodies))
+	copy(out, s.networkBodies)
+	return out
+}
+
+func (s *BufferStore) webSocketEventsCopy() []WebSocketEvent {
+	if len(s.wsEvents) == 0 {
+		return []WebSocketEvent{}
+	}
+	out := make([]WebSocketEvent, len(s.wsEvents))
+	copy(out, s.wsEvents)
+	return out
+}
+
+func (s *BufferStore) enhancedActionsCopy() []EnhancedAction {
+	if len(s.enhancedActions) == 0 {
+		return []EnhancedAction{}
+	}
+	out := make([]EnhancedAction, len(s.enhancedActions))
+	copy(out, s.enhancedActions)
+	return out
+}
+
+func (s *BufferStore) clearNetworkBuffers() {
+	s.networkBodies = make([]NetworkBody, 0)
+	s.networkAddedAt = make([]time.Time, 0)
+	s.networkTotalAdded = 0
+	s.networkErrorTotalAdded = 0
+	s.networkBodyMemoryTotal = 0
+}
+
+func (s *BufferStore) clearWebSocketBuffers() {
+	s.wsEvents = make([]WebSocketEvent, 0)
+	s.wsAddedAt = make([]time.Time, 0)
+	s.wsTotalAdded = 0
+	s.wsMemoryTotal = 0
+}
+
+func (s *BufferStore) clearActionBuffers() {
+	s.enhancedActions = make([]EnhancedAction, 0)
+	s.actionAddedAt = make([]time.Time, 0)
+	s.actionTotalAdded = 0
+}
+
+func (s *BufferStore) clearAllEventBuffers() {
+	s.wsEvents = make([]WebSocketEvent, 0)
+	s.wsAddedAt = make([]time.Time, 0)
+	s.wsMemoryTotal = 0
+	s.networkBodies = make([]NetworkBody, 0)
+	s.networkAddedAt = make([]time.Time, 0)
+	s.networkBodyMemoryTotal = 0
+	s.enhancedActions = make([]EnhancedAction, 0)
+	s.actionAddedAt = make([]time.Time, 0)
+}
+
+func (s *BufferStore) networkTotal() int64 {
+	return s.networkTotalAdded
+}
+
+func (s *BufferStore) networkErrorTotal() int64 {
+	return s.networkErrorTotalAdded
+}
+
+func (s *BufferStore) webSocketTotal() int64 {
+	return s.wsTotalAdded
+}
+
+func (s *BufferStore) actionTotal() int64 {
+	return s.actionTotalAdded
+}
+
+func (s *BufferStore) networkCount() int {
+	return len(s.networkBodies)
+}
+
+func (s *BufferStore) webSocketCount() int {
+	return len(s.wsEvents)
+}
+
+func (s *BufferStore) actionCount() int {
+	return len(s.enhancedActions)
 }
